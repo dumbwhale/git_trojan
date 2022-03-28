deea2828144ec1750f53f8a4fa934786b1407ea6
dumbwhale <97585113+dumbwhale@users.noreply.github.com>
2022-03-28 09:28:41 +0800

    Update environment.py

--- modules/environment.py
+++ modules/environment.py
@@ -3,8 +3,6 @@
 import os
 
 def run(**args):
-    print "[*] In Environment module"
+    print ("[*] In Environment module")
     return str(os.environ)
     
-if __name__ == '__main__':
-            run()
